e810fec2b340f64056f8aef4b9e5b6ed7357cf58
muzzii255 <muzammil255@gmail.com>
2024-10-02 15:39:54 -0400

    test

--- test.py
+++ test.py
@@ -2,55 +2,35 @@
 import pdfkit
 import os
 import datetime
-def image_convertor(plaintiff_name,lein):
-    # exe_loc = f'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'
-    # config = pdfkit.configuration(wkhtmltopdf=exe_loc)
-    # options = {
-    # "enable-local-file-access": "",
-    # 'footer-right': "[page]/[topage]",
-    # 'footer-font-name': 'Sans Pro',
-    # 'footer-font-size': 8,
-    # 'footer-spacing':2,
-    # 'footer-left': 'https://apps.nyc.gov/nyc-mailform/validation',
-    # 'header-font-name': 'Sans Pro',
-    # 'header-font-size': 8,
-    # 'header-spacing':2,
-    # 'header-left': datetime.datetime.now().strftime('%m/%#d/%y, %H:%M%p'),
-    # 'header-center': 'Thank you',
-    # 'user-style-sheet': 'formsubmit.css'
-    # }
-    options = {
-        'enable-local-file-access': None,  # Enable access to local files like CSS/JS
-        'page-size': 'A4',
-        'margin-top': '0.05in',
-        'margin-right': '0.05in',
-        'margin-bottom': '0.05in',
-        'margin-left': '0.05in',
-        'encoding': "UTF-8",
-        'no-outline': None,
-        'footer-right': "[page]/[topage]",
-        'footer-font-name': 'Sans Pro',
-        'footer-font-size': 8,
-        'footer-spacing':2,
-        'footer-left': 'https://apps.nyc.gov/nyc-mailform/validation',
-        'header-font-name': 'Sans Pro',
-        'header-font-size': 8,
-        'header-spacing':2,
-        'header-left': datetime.datetime.now().strftime('%m/%#d/%y, %H:%M%p'),
-        'header-center': 'Thank you',
         
-    }
+        
+options = {
+    'enable-local-file-access': None,  # Enable access to local files like CSS/JS
+    'page-size': 'A4',
+    'margin-top': '0.05in',
+    'margin-right': '0.05in',
+    'margin-bottom': '0.05in',
+    'margin-left': '0.05in',
+    'encoding': "UTF-8",
+    'no-outline': None,
+    'footer-right': "[page]/[topage]",
+    'footer-font-name': 'Sans Pro',
+    'footer-font-size': 8,
+    'footer-spacing':2,
+    'footer-left': 'https://apps.nyc.gov/nyc-mailform/validation',
+    'header-font-name': 'Sans Pro',
+    'header-font-size': 8,
+    'header-spacing':2,
+    'header-left': datetime.datetime.now().strftime('%m/%-d/%y, %H:%M%p'),
+    'header-center': 'Thank you',
+    
+}
 
-    current_cwd = os.getcwd()
-    try:
-        pdfkit.from_file(f'test.html',f'{plaintiff_name} {lein} Medicaid Submission Form.pdf',options=options)
-    except Exception as e:
-        print(e)
-        
-        
-# print(datetime.datetime.now().strftime('%m/%#d/%y, %H:%M%p'))
-image_convertor('test1 test2','test2')
-
+current_cwd = os.getcwd()
+try:
+    pdfkit.from_file('test.html','test.pdf',options=options)
+except Exception as e:
+    print(e)
 
 
 
